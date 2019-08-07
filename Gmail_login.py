22d52f5e4f0c6ea2ef5e1e6ff3922396397a28f9
shubham <shubhamaggarwal1603@gmail.com>
2019-08-07 17:08:26 +0530

    First commit

---
+++ Gmail_login.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+usernameStr = 'shubhamaggarwal1603@gmail.com'
+passwordStr = 'Iamsexyanduknowit@'
+
+browser = webdriver.Chrome('/home/user8/Desktop/selenium project/driver/chromedriver')
+browser.get(('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'))
+
+# fill in username and hit the next button
+username = browser.find_element_by_xpath('//*[@id="identifierId"]')
+username.send_keys(usernameStr)
+
+nextButton = browser.find_element_by_xpath('//*[@id="identifierNext"]/span/span')
+nextButton.click()
+time.sleep(3) 
+# if nextButton:
+    # nextButton[0].click()
+
+
+# another method
+# time.sleep(3)    //*[@id="password"]/div[1]/div/div[1]/input
+# password = browser.find_element_by_id('Passwd')
+
+# wait for transition then continue to fill items
+# wait = WebDriverWait(browser,5)
+##password = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'whsOnd zHQkBf')))
+
+
+# pas= browser.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys('Iniohdwn10hx@')
+pas= browser.find_element_by_name('password').send_keys(passwordStr)
+
+
+
+nextButton = browser.find_element_by_xpath('//*[@id="passwordNext"]/span/span')
+nextButton.click()
+
+
+
+
---
+++ linkedIn_login.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+import bs4
+import pandas as pd
+import re
+import time
+import requests
+
+from urllib.request import urlopen as uReq
+from urllib.request import Request
+from bs4 import BeautifulSoup as soup
+
+# specifies the path to the chromedriver.exe
+driver = webdriver.Chrome('/home/user8/Desktop/chromedriver')
+
+# driver.get method() will navigate to a page given by the URL address
+driver.get('https://www.linkedin.com')
+
+Sign_in_button = driver.find_element_by_class_name('nav__button-secondary')
+
+# .click() to mimic button click
+Sign_in_button.click()
+
+# locate email form by_class_name
+username = driver.find_element_by_id('username')
+
+# send_keys() to simulate key strokes
+username.send_keys('shubhamaggarwal0316@gmail.com')
+
+# locate password form by_class_name
+password = driver.find_element_by_id('password')
+
+# send_keys() to simulate key strokes
+password.send_keys('Linke11dad23e')
+
+# locate submit button by_class_name
+sign_in_button = driver.find_element_by_class_name('login__form_action_container')
+
+# .click() to mimic button click
+sign_in_button.click()
